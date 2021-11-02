178ed1f56377fd1f66a2e33a303bfcffb810be45
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-11-02 00:54:39 -0300

    feat: adciona inputkey para o menu

--- game/menu.lua
+++ game/menu.lua
@@ -1,8 +1,7 @@
 local menu = {}
 
 function menu:comecar() 
-    -- print(" 1 - começar \n 2 - opções \n 3 - sair");
-    love.graphics.print(' 1 - começar \n 2 - opções \n 3 - sair', 400, 300);
+    
 end 
 
 return menu
--- main.lua
+++ main.lua
@@ -1,5 +1,35 @@
 menu = require("game/menu");
 
-function love.draw() 
-    menu.comecar();
+
+local state = 'MENU'
+
+
+function love.update (dt)
+    
+end
+
+
+function love.draw()
+    if state == 'MENU' then
+        love.graphics.print('1 - Começar \n2 - Opções \nEsc - Sair', 400, 300);
+    elseif state == 'JOGAR' then
+        love.graphics.print('Jogo \nM - menu', 400, 300);
+    elseif state == 'OPCOES' then
+        love.graphics.print('Opções \nM - menu', 400, 300);        
+    end 
+end
+
+
+function love.keypressed(key)
+    print(key);
+
+    if key == "escape" then
+        love.event.quit();
+    elseif key == "1" then
+        state = 'JOGAR'
+    elseif key == "2" then
+        state = 'OPCOES'
+    elseif key == 'm' then
+        state = 'MENU'
+    end
 end 

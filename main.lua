035f7b0dbeb228ead07b01d7cfc644158cee0644
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-11-02 09:21:50 -0300

    feat: adiciona debug com posição do inseto, velocidade e fps

--- main.lua
+++ main.lua
@@ -10,11 +10,18 @@ VELOCIDADE_INSETO = 200;
 
 local player = 0; 
 
-
+local debugMode = {}
+function debugMode:render() 
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10);
+    love.graphics.print("player X: ".. tostring(player.x), 10, 25); 
+    love.graphics.print("player Y: ".. tostring(player.y), 10, 40); 
+    love.graphics.print("player DX: ".. tostring(player.dx), 10, 65); 
+    love.graphics.print("player DY: ".. tostring(player.dy), 10, 80); 
+end
+ 
 function love.update (dt)
     if state == 'JOGAR' then
         
-
         if love.keyboard.isDown('s') then
             player.dy = VELOCIDADE_INSETO;
         elseif love.keyboard.isDown('w') then
@@ -33,6 +40,7 @@ function love.update (dt)
 end
 
 function love.load() 
+    love.window.setTitle("jogo da formiga");
     player = inseto
     player.init(0, 400, 300, 20, 20);
 end
@@ -43,18 +51,18 @@ function love.draw()
     if state == 'MENU' then
         love.graphics.print('1 - Começar \n2 - Opções \nEsc - Sair', 400, 300);
         if debug == 'on' then
-            love.graphics.print('É o debugs', 250, 400);        
+            debugMode.render();
         end
     elseif state == 'JOGAR' then
         love.graphics.print('M - menu', 700, 10);
         player.render();
         if debug == 'on' then
-            love.graphics.print('É o debugs', 250, 400);        
+            debugMode.render()  
         end
     elseif state == 'OPCOES' then
         love.graphics.print('Opções \nM - menu', 400, 300);
         if debug == 'on' then
-            love.graphics.print('É o debugs', 250, 400);        
+            debugMode.render(); 
         end
     end 
 end

b801f6c20219d48acb095fe811d5d651daa8bd07
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-11-16 20:22:02 -0300

    feat: adiciona metodo load para carregar levels

--- game/scenes/levels/1/level1.lua
+++ game/scenes/levels/1/level1.lua
@@ -1,12 +1,15 @@
 require('game/objects/parede');
 
-local structure = {
-    teste1 = Parede:new(100, 200, 200, 40, 1, 0.5, 0);
-    teste = Parede:new(100, 400, 200, 10, 1, 0, 0);
-}
+local structure = {};
+Level1_structure = {};
 
-Level1_structure = {}
 
+function Level1_structure:load()
+    structure = {
+        teste1 = Parede:new(100, 200, 200, 40, 1, 0.5, 0);
+        teste = Parede:new(100, 400, 200, 10, 1, 0, 0);
+    }
+end
 
 function Level1_structure:disableAllColisions()
     for paredes, bloco in pairs(structure) do

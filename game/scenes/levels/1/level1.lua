c62f55fd39ee793c87de5462f18692a39ff02d71
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-11-15 21:57:48 -0300

    refactor: melhora funcionamento das estruturas

--- game/scenes/levels/1/level1.lua
+++ game/scenes/levels/1/level1.lua
@@ -1,31 +1,27 @@
 require('game/objects/parede');
 
-Level1_structure = {
+local structure = {
     teste1 = Parede:new(100, 200, 200, 40, 1, 0.5, 0);
     teste = Parede:new(100, 400, 200, 10, 1, 0, 0);
 }
 
+Level1_structure = {}
+
+
 function Level1_structure:disableAllColisions()
-    for paredes, bloco in pairs(Level1_structure) do
-        if not (paredes == 'disableAllColisions' or paredes == 'drawAll' or paredes == 'enableAllColisions') then
-            bloco:disableColision();
-        end
+    for paredes, bloco in pairs(structure) do
+        bloco:disableColision();
     end    
 end
 
 function Level1_structure:enableAllColisions()
-    for paredes, bloco in pairs(Level1_structure) do
-        if not (paredes == 'disableAllColisions' or paredes == 'drawAll' or paredes == 'enableAllColisions') then
-            bloco:enableColision();
-        end
+    for paredes, bloco in pairs(structure) do
+        bloco:enableColision();
     end     
 end
 
-
 function Level1_structure:drawAll()
-    for paredes, bloco in pairs(Level1_structure) do
-        if not (paredes == 'drawAll' or paredes == 'disableAllColisions' or paredes == 'enableAllColisions') then        
-            bloco:draw();
-        end
+    for paredes, bloco in pairs(structure) do
+        bloco:draw();
     end    
 end

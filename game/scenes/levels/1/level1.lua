d85d6f7eb94ba8a17fe9bff6c2a4fec8a5641af1
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-11-17 14:44:54 -0300

    feat: adiciona luz que o BESOURO não conseque passar

--- game/scenes/levels/1/level1.lua
+++ game/scenes/levels/1/level1.lua
@@ -1,35 +1,44 @@
 require('game/objects/parede');
 require('game/objects/folha');
 
-local structure = {};
+local paredes = {};
+local luzes = {};
 Level1_structure = {};
 
 
 function Level1_structure:load()
-    structure = {
+    paredes = {
         teste1 = Parede:new(100, 200, 200, 40, 1, 0.5, 0);
         teste = Parede:new(100, 400, 200, 10, 1, 0, 0);
+    }
+
+    luzes = {
+        teste = Parede:new(200, 600, 200, 10);
     }
 
     Folha:load();
 end
 
 function Level1_structure:disableAllColisions()
-    for paredes, bloco in pairs(structure) do
+    for i, bloco in pairs(paredes) do
         bloco:disableColision();
     end    
 end
 
 function Level1_structure:enableAllColisions()
-    for paredes, bloco in pairs(structure) do
+    for i, bloco in pairs(paredes) do
         bloco:enableColision();
     end     
 end
 
 function Level1_structure:draw()
-    for paredes, bloco in pairs(structure) do
+    for i, bloco in pairs(paredes) do
         bloco:draw();
     end  
+
+    for i, luzes in pairs(luzes) do 
+        luzes:draw();
+    end
 
     Folha:draw();  
 end
